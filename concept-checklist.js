675ee56e7544ae8bc6ac2a037eb63ae9f1606012
engrasel <contact.engrasel@gmail.com>
2021-08-09 00:21:34 +0600

    first commit

---
+++ concept-checklist.js
@@ -0,0 +1,133 @@
+// 1st check list
+/* const num = 123;
+const name = "Rasel";
+const married = false; */
+
+
+// 2nd check list
+/* let age = 22;
+const name2 = "rasel";
+age = 24; */
+
+
+// 3rd check list
+/* const num1 = 10;
+const num2 = 2;
+const totall = num1 + num2;
+const totall = num1 - num2;
+const totall = num1 * num2;
+const totall = num1 / num2;
+const totall = num1 % num2;
+const totall = num1 % num2;
+console.log(totall); */
+
+
+// 4th check list
+/* const num1 = 5;
+const num2 = 10;
+if (num1 < num2) {
+    console.log('number one is smaller');
+}
+if (num2 > num1) {
+    console.log('number two is bigger');
+}
+if (num1 == num2) {
+    console.log('number one and number two is same value');
+}
+if (num1 != num2) {
+    console.log('number one and number two is not same value');
+}
+if (num2 <= num1) {
+    console.log('number two is small or equal');
+}
+if (num2 >= num1) {
+    console.log('number two is bigger or equal');
+} */
+
+// 5th check list
+/* const age = 26;
+const married = false;
+if (age > 18 && age <=25 ) {
+    console.log('i want marry now');
+}
+else if (married == false) {
+    console.log('i want marry now too');
+} */
+
+// 6th check list
+/* function marks(number) {
+    if (number < 33) {
+        return 'you are fail';
+    }
+    else if (number >= 33 && number < 50) {
+        return 'your are B+'
+    }
+    else if (number >= 50 && number <= 100) {
+        return 'your are A+'
+    }
+    else {
+        return 'you are grade is not found';
+    }
+}
+const inputNumber = marks(120);
+console.log(inputNumber); */
+
+
+
+// 7th check list
+/* let i = 0;
+while (i < 19) {
+    i++;
+    if (i % 2 == 1) {
+        console.log('this is a even number', i);
+    }
+} */
+
+
+// 8th check list
+/* const students = [41, 35, 86, 83, 16, 34, 76, 64, 45, 12];
+const element = students.length;
+students[4] = 32;
+students.push(97);
+students.shift();
+const findNumber = students.indexOf(64);
+console.log('Totall Elements Number is', element);
+console.log(students);
+console.log('the 64 number are index of', findNumber); */
+
+// 9th check list
+/* let students = [45, 86, 35, 16, 35, 86, 75, 23, 15, 65];
+for (i = 0; i < students.length; i++){
+    const totallStudent = students[i];
+    console.log(totallStudent);
+}
+ */
+
+// 10th check list
+/* var array = [3 , 6, 2, 56, 32, 5, 89, 32];
+var largest= 0;
+
+for (i=0; i<=largest;i++){
+    if (array[i]>largest) {
+        var largest=array[i];
+    }
+}
+console.log(largest); */
+
+
+// 11th check list
+/* function myFunc(num1, num2, num3) {
+    const totall = num1 * num2 * num3;
+    return totall;
+}
+const inputNumber = myFunc(5, 5, 2);
+console.log(inputNumber); */
+
+// 12th check list
+/* const student = {
+    name: 'rasel',
+    age: 24,
+    dob: '09-04-1997',
+};
+student.name = 'Shakil';
+console.log(student); */
